dd0619aa08691bb364d4ef2efd4fa996b5d70fac
unknown <sarthakkaushik090@gmail.com>
2021-07-23 09:52:25 +0530

    first commit

---
+++ react-snake/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="container mx-auto">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio praesentium
+      tenetur ratione facilis eaque hic dolor quisquam blanditiis. At
+      perferendis molestias quidem, officia explicabo quos voluptates eveniet
+      impedit? Totam, eveniet?
+    </div>
+  );
+}
+
+export default App;
